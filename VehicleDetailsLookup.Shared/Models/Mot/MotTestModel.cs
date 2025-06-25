4ca7afd90c0600b2cf1896d564226c60c7148774
Dylan Irwin <Dylan.Irwin@kerridgecs.com>
2025-06-25 16:08:30 +0100

    Fixed duplicate defects Started work on ordering MOT tests Need to maintain order from API, so probably need Index column

--- VehicleDetailsLookup.Shared/Models/Mot/MotTestModel.cs
+++ VehicleDetailsLookup.Shared/Models/Mot/MotTestModel.cs
@@ -2,11 +2,12 @@
 {
     public class MotTestModel : IMotTestModel
     {
+        public string TestNumber { get; set; } = string.Empty;
         public DateOnly CompletedDate { get; set; }
         public bool Passed { get; set; }
         public DateOnly ExpiryDate { get; set; }
         public long OdometerValue { get; set; }
         public string OdometerUnit { get; set; } = string.Empty;
-        public IEnumerable<IMotDefectModel> Defects { get; set; } = [];
+        public IEnumerable<MotDefectModel> Defects { get; set; } = [];
     }
 }
