c02c23a5da5c9aa83b37dcf12514ed379e279d40
Dylan Irwin <Dylan.Irwin99@gmail.com>
2025-07-03 21:15:34 +0100

    Added MOT not yet due for vehicles under 3 years old

--- VehicleDetailsLookup.Client/Components/UI/VehicleDetails/MotStatus/MotStatus.razor.cs
+++ VehicleDetailsLookup.Client/Components/UI/VehicleDetails/MotStatus/MotStatus.razor.cs
@@ -9,6 +9,7 @@ namespace VehicleDetailsLookup.Client.Components.UI.VehicleDetails.MotStatus
         public IDetailsModel? Details { get; set; }
 
         private string _statusText = string.Empty;
+        private string _dateText = string.Empty;
         private string _style = string.Empty;
 
         protected override void OnParametersSet()
@@ -19,22 +20,32 @@ namespace VehicleDetailsLookup.Client.Components.UI.VehicleDetails.MotStatus
             {
                 case Shared.Models.Enums.MotStatus.Valid:
                     _statusText = "Valid";
+                    _dateText = "Expires: " + Details?.MotExpiryDate;
                     _style = "background-color: var(--mud-palette-success);";
                     break;
                 case Shared.Models.Enums.MotStatus.Invalid:
                     _statusText = "Invalid";
+                    _dateText = "Expired: " + Details?.MotExpiryDate;
                     _style = "background-color: var(--mud-palette-error);";
-                    break;
-                case Shared.Models.Enums.MotStatus.NoDetails:
-                    _statusText = "No details held by DVLA";
-                    _style = "background-color: var(--mud-palette-gray-default);";
                     break;
                 case Shared.Models.Enums.MotStatus.NoResults:
                     _statusText = "No results found";
+                    _dateText = string.Empty;
                     _style = "background-color: var(--mud-palette-gray-default);";
                     break;
-                default:
-                    _statusText = "Unknown";
+                case Shared.Models.Enums.MotStatus.NoDetails:
+
+                    if (Details?.MonthOfFirstRegistration > DateOnly.FromDateTime(DateTime.Now.AddYears(-3)))
+                    {
+                        _statusText = "MOT not yet due";
+                        _dateText = "Due by: " + Details.MonthOfFirstRegistration.AddYears(3);
+                    }
+                    else
+                    {
+                        _statusText = "No details held by DVLA";
+                        _dateText = string.Empty;
+                    }
+
                     _style = "background-color: var(--mud-palette-gray-default);";
                     break;
             }
