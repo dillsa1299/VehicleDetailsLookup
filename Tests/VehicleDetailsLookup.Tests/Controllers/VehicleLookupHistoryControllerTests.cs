4be4c7e99ac3ac698ac83b2399909b52347b5568
Dylan Irwin <Dylan.Irwin99@gmail.com>
2026-01-11 17:38:58 +0000

    Updates CICD for code coverage

---
+++ Tests/VehicleDetailsLookup.Tests/Controllers/VehicleLookupHistoryControllerTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleDetailsLookup.Tests.Controllers
+{
+    internal class VehicleLookupHistoryControllerTests
+    {
+    }
+}
